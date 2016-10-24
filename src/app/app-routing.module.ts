c14eb13d18e738294ba66344f8198d8faa2edca7
Kim Maida <kim.maida@auth0.com>
2016-10-24 14:36:07 -0400

    Add Home, About, and Router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }     from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent }  from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+			{ 
+				path: '', 
+				component: HomeComponent
+			},
+      { 
+				path: 'about', 
+				component: AboutComponent
+			}
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavControlDirective } from './header/nav-control.directive';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavControlDirective
+    NavControlDirective,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+		AppRoutingModule
   ],
   providers: [
 	  Title
