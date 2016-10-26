f6562b25a41d7cdf137b8e67485664eb6eecd919
Kim Maida <kim.maida@auth0.com>
2016-10-26 13:47:31 -0400

    Simplify footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-  thisYear: number;
-
-  ngOnInit() {
-    this.thisYear = new Date().getFullYear();
-  }
-
-}
+export class FooterComponent {}
