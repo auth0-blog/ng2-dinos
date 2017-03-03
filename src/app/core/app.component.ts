44c226dc3d5c8b0a573a63523927a6ffc7473dc2
Kim Maida <kim.maida@auth0.com>
2017-03-03 11:32:48 -0500

    More fixes from updating to angular-cli rc.0: delete index.ts, fix path to tsconfig, declare window.

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
+declare var window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
